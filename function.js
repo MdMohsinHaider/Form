5161b4f91d3ead26f9b725b3ec4ab884d84a303f
Mohsin <mdmohsinhaider6@gmail.com>
2023-03-22 00:42:46 +0530

    init git repo

---
+++ function.js
@@ -0,0 +1,10 @@
+
+function takevalue(){
+    var name=document.getElementById("name").value;
+    var mobile=document.getElementById("mobile").value;
+    var password=document.getElementById("password").value;
+            
+    document.write("your name is "+name +"<br>");
+    document.write("your mobile number is "+mobile +"<br>");
+    document.write("your password is "+password);
+    }
